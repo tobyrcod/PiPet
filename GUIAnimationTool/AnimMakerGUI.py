03eaba4dd8afcf58bf183bc23eb32f399cedcfa0
tobyrcod <61169763+tobyrcod@users.noreply.github.com>
2022-04-10 15:05:28 +0100

    8x8 Canvas
    - 8x8 Grid represented by 2D colour array
    - Drawing to screen

--- GUIAnimationTool/AnimMakerGUI.py
+++ GUIAnimationTool/AnimMakerGUI.py
@@ -2,13 +2,54 @@
 # https://www.youtube.com/watch?v=N20eXcfyQ_4
 from utils import *
 
+# Pygame variables
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("PiPet Animation Maker")
+clock = pygame.time.Clock()
 
-run = True
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
 
-pygame.quit()
+def init_grid(rows, cols, start_color):
+    grid = []
+
+    for i in range(0, rows):
+        grid.append([])
+        for _ in range(0, cols):
+            grid[i].append(start_color)
+
+    return grid
+
+
+# TODO: Draw the grid to a different canvas instead of directly to the main display
+def draw(win, grid):
+    def draw_grid(WIN, grid):
+        for y, row in enumerate(grid):
+            for x, cell_color in enumerate(row):
+                pygame.draw.rect(WIN, cell_color, (
+                    x * PIXEL_SIZE, y * PIXEL_SIZE,  # (x, y) position of the cell
+                    PIXEL_SIZE, PIXEL_SIZE  # the size of the cell
+                ))
+
+    win.fill(BG_COLOR)
+    draw_grid(WIN, grid)
+    pygame.display.update()
+
+
+# Main game Loop
+def main():
+
+    grid = init_grid(ROWS, COLS, BLUE)
+
+    run = True
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        draw(WIN, grid)
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
--- GUIAnimationTool/utils/settings.py
+++ GUIAnimationTool/utils/settings.py
@@ -6,8 +6,8 @@ pygame.font.init()
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
-BLUE = (0, 255, 0)
-GREEN = (0, 0, 255)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 
 # Pygame Settings
 FPS = 60
