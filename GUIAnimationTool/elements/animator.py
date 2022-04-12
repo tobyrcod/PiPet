d7d386f01fc046688956ad0b099ced8e5c9a7309
tobyrcod <61169763+tobyrcod@users.noreply.github.com>
2022-04-12 20:34:35 +0100

    Scrollbar viewport and content visualisation

--- GUIAnimationTool/elements/animator.py
+++ GUIAnimationTool/elements/animator.py
@@ -11,13 +11,16 @@ class Animator:
         self.frames = []
         self.events = AnimatorEvents()
 
-        self.animator_frame_rect = pygame.Rect(0, 0, FRAME_WIDTH, (self.rect.height - 3 * FRAME_PADDING) * 0.8)
+        self.animator_frame_rect = pygame.Rect(0, 0, FRAME_WIDTH, (self.rect.height - 3 * FRAME_PADDING) * 0.9)
         self.animator_frames = []
         self.active_animator_frame_index = -1
 
         new_frame_button_rect = pygame.Rect(0, 0, FRAME_WIDTH * 0.7, FRAME_WIDTH * 0.7)
         self.new_frame_button = Button(new_frame_button_rect, WHITE, "New")
         self.new_frame_button.events.on_clicked += self.add_new_frame
+
+        bar_rect = pygame.Rect(FRAME_PADDING, 2 * FRAME_PADDING + self.animator_frame_rect.height, self.rect.width - 2 * FRAME_PADDING, (self.rect.height - 3 * FRAME_PADDING) * 0.1)
+        self.scrollbar = ScrollBar(bar_rect)
 
     def init(self):
         self.add_new_frame()
@@ -56,27 +59,33 @@ class Animator:
         # Background
         animator_surface.fill(WHITE)
 
+        # Scroll Area
+        viewport_rect = pygame.Rect(FRAME_PADDING, FRAME_PADDING, self.rect.width - FRAME_PADDING * 2, self.rect.height - FRAME_PADDING * 3 - self.scrollbar.bar_rect.height)
+        viewport_surface = pygame.Surface(viewport_rect.size)
+        viewport_surface.fill(GREEN)
+        animator_surface.blit(viewport_surface, viewport_rect)
+
+        content_rect = pygame.Rect(FRAME_PADDING, FRAME_PADDING, len(self.animator_frames) * (self.animator_frame_rect.width + FRAME_PADDING) + self.new_frame_button.rect.width, viewport_rect.height)
+        content_surface = pygame.Surface(content_rect.size)
+        content_surface.fill(BLUE)
+        animator_surface.blit(content_surface, content_rect)
         # All the frames
         i = -1
         for i, animator_frame in enumerate(self.animator_frames):
-            animator_frame.rect.topleft = (FRAME_PADDING + i * (FRAME_WIDTH + FRAME_PADDING), FRAME_PADDING)
+            animator_frame.rect.topleft = (FRAME_PADDING + i * (FRAME_WIDTH + FRAME_PADDING) - self.scrollbar.offset, FRAME_PADDING)
 
             animator_frame_surface = animator_frame.get_surface(self.frames, i == self.active_animator_frame_index)
             animator_surface.blit(animator_frame_surface, animator_frame.rect)
 
         # Add new frame button
         i += 1
-        self.new_frame_button.rect.topleft = (FRAME_PADDING + i * (FRAME_WIDTH + FRAME_PADDING), FRAME_PADDING + self.animator_frame_rect.height / 2 - self.new_frame_button.rect.height / 2)
+        self.new_frame_button.rect.topleft = (FRAME_PADDING + i * (FRAME_WIDTH + FRAME_PADDING) - self.scrollbar.offset, FRAME_PADDING + self.animator_frame_rect.height / 2 - self.new_frame_button.rect.height / 2)
         new_frame_button_surface = self.new_frame_button.get_surface()
         animator_surface.blit(new_frame_button_surface, self.new_frame_button.rect)
 
-        # Text to say which element this is
-        font = get_font(size=30)
-        text_surface = font.render("Animator", 1, BLACK)
-        animator_surface.blit(text_surface, (
-            0,
-            self.rect.height - text_surface.get_height()
-        ))
+        # Scroll bar
+        scrollbar_surface = self.scrollbar.get_surface()
+        animator_surface.blit(scrollbar_surface, self.scrollbar.bar_rect)
 
         return animator_surface
 
@@ -103,6 +112,22 @@ class AnimatorFrame(IClickable):
             pygame.draw.rect(animator_frame_surface, RED, (0, 0, *self.rect.size), 5)
 
         return animator_frame_surface
+
+
+class ScrollBar:
+    def __init__(self, bar_rect):
+        self.bar_rect = bar_rect
+        self.offset = 0
+
+    def calculate_offset(self):
+        self.offset = 0
+
+    def get_surface(self):
+
+        scrollbar_surface = pygame.Surface(self.bar_rect.size)
+        scrollbar_surface.fill(BLACK)
+
+        return scrollbar_surface
 
 
 class AnimatorEvents(Events):
